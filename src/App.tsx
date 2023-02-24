ecad019773d781bea0c13930d9f7a84476f7e8f6
omar-home-dekstop <medzhidov.omar11@gmail.com>
2023-02-24 13:07:48 +0300

    banner and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import styled from "styled-components";
+import special from "./assets/special.jpg";
+import Header from "./components/Header";
 
+const Container = styled.div`
+  max-width: calc(1440px - 2 * 140px);
+  margin: 0 auto;
+  border: 1px solid black;
+
+  .special_img {
+    margin: 0 -100px;
+  }
+`;
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Header />
+        <img className="special_img" src={special} alt="" />
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
